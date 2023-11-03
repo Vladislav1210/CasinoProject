45184c9683904195a2e78dcad18b8bee03d7bb28
Vladislav1210 <misun_vlad@mail.ru>
2023-11-03 20:59:21 +0300

    plus one commit

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import sys
 import sqlite3
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog
 from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
+from random import choice
 
-NUMBER = ['1', '2', '3', '4', '5', '6', '7', '7', '8', '9', '0']
-SLOTS_TYPES = ['']
+NUMBER = ['1', '2', '3', '4', '5', '6', '7', '7', '8', '9', '0']  # цифирки
+
+SLOTS_TYPES = ['7', '7', 'bar', 'bar', 'bar', 'b', 'b', 'b', 'b', 'g', 'g', 'g',
+               'g', 'g', 'l', 'l', 'l', 'l', 'l', 'l', 'c', 'c', 'c', '.', '.', '.', '.']  # для вероятности
+TROLL_PHRASES = ['Сегодня не твой день', 'Неудачник', 'Деньги - прощайте',
+                 'Банкротство - здравствуй', 'На улице тоже есть чем питаться',
+                 'Может в другой раз повезёт', 'Терпи', 'Неповезло(',
+                 'Ещё одна неудача', 'Какой же позор!']  # при проигрыше
+
+VICTORY_PHRASES = ['И у нас победитель!', 'Мне кажется ты жульничал',
+                   'Кто бы мог поверить?!', 'Jackpot!!!', 'Судьба тебя одарила']  # при выйгрыше
 
 
 class Start_Window(QMainWindow):
@@ -20,6 +30,13 @@ class Start_Window(QMainWindow):
         self.rw = Register_Window()
         self.setCentralWidget(self.rw)
         self.rw.show()
+        self.sw = Start_Window()
+        self.sw.setVisible(False)
+
+    def enter_window(self):
+        self.ew = Enter_Window()
+        self.setCentralWidget(self.ew)
+        self.ew.show()
         self.sw = Start_Window()
         self.sw.setVisible(False)
 
@@ -58,7 +75,7 @@ class Register_Window(QMainWindow):
         elif ' ' in self.login:
             self.name_error.setText('Логин не должен содержать пробелы')
             self.flag = False
-        elif self.login in cur.execute("SELECT login from client_data").fetchone():
+        elif self.login in cur.execute("SELECT login from client_data").fetchall():
             self.name_error.setText('Данный логин уже существует')
             self.flag = False
         f.close()
@@ -115,17 +132,226 @@ class Register_Window(QMainWindow):
             self.rw.setVisible(False)
 
 
+class Enter_Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Casino_project3.ui', self)
+        self.setFixedSize(651, 761)
+        self.pict2.setPixmap(QPixmap('White_Casino_background.png'))
+
+        self.finish_ent.clicled.connect(self.enter_finish)
+
+    def enter_finish(self):
+        self.flag = True
+
+        self.login = self.enter_name.text()
+        self.password = self.enter_password.text()
+
+        self.name_error.setText('')
+        self.password_error.setText('')
+
+        f = sqlite3.connect('Casino_database.db')
+        cur = f.cursor()
+        if self.login not in cur.execute("SELECT login from client_data").fetchall():
+            self.flag = False
+            self.name_error.setText('Такого логина не существует')
+        elif self.password != cur.execute(f"""SELECT password from client_data 
+        WHERE login = '{self.login}'""").fetchone():
+            self.flag = False
+            self.password_error.setText('Неверный пароль')
+        f.close()
+        if self.flag:
+            f = sqlite3.connect('Casino_database.db')
+            cur = f.cursor()
+            balance = cur.execute(f"""SELECT balance from client_data 
+                    WHERE login = '{self.login}'""").fetchone()
+            self.mw = Main_Window(self, self.login, int(balance))
+            self.setCentralWidget(self.mw)
+            self.mw.show()
+            self.ew = Enter_Window()
+            self.ew.setVisible(False)
+
+
+
 class Main_Window(QMainWindow):
     def __init__(self, *arg):
         super().__init__()
-        self.login = arg[1]
-        self.balance = arg[2]
+        self.login = 'aboba'
+        self.balance = 0  # должно быть всегда int
         uic.loadUi('Casino_project4.ui', self)
         self.setFixedSize(651, 761)
         self.pict4.setPixmap(QPixmap('Casino_patern.png'))
+
         self.login_label.setText(self.login)
         self.balance_label.setText(f'Ваш баланс: {self.balance}')
+        self.spin_error.setText('')
+        self.troll_label.setText('')
+        self.info_label.setText('')
+        self.slot1.setText('.')
+        self.slot2.setText('.')
+        self.slot3.setText('.')
 
+        self.replenish_button.clicked.connect(self.replenish_method)  # Пополнить
+        self.withdraw_button.clicked.connect(self.withdraw_method)  # Снять
+        self.spin_button.clicked.connect(self.spin)  # spin
+        self.exit_button.clicked.connect(self.exit)  # выйти
+
+    def replenish_method(self):
+        value, ok_pressed = QInputDialog.getInt(self, 'Пополнение',
+                                                'Введите пополняемую сумму', 100, 100, 1000000, 100)
+        if ok_pressed:
+            f = sqlite3.connect('Casino_database.db')
+            cur = f.cursor()
+            cur.execute(f"""UPDATE client_data SET balance = balance + {str(value)} WHERE login = '{self.login}'""")
+            f.commit()
+            self.balance = int(*cur.execute(f"""SELECT balance from client_data 
+            where login = '{self.login}'""").fetchone())
+            f.close()
+            self.balance_label.setText(f'Ваш баланс: {self.balance}')
+
+    def withdraw_method(self):
+        f = sqlite3.connect('Casino_database.db')
+        cur = f.cursor()
+        value, ok_pressed = QInputDialog.getInt(self, 'Снятие', 'Введите снимаемую сумму', 50, 50,
+                                                int(*cur.execute(f"""SELECT balance from client_data WHERE login = 
+                                                '{self.login}'""").fetchone()), 100)
+        if ok_pressed:
+            cur.execute(f"""UPDATE client_data SET balance = balance - {str(value)} WHERE login = '{self.login}'""")
+            f.commit()
+            self.balance = int(*cur.execute(f"""SELECT balance from client_data 
+                        where login = '{self.login}'""").fetchone())
+            self.balance_label.setText(f'Ваш баланс: {self.balance}')
+        f.close()
+
+    def exit(self):
+        self.sw = Start_Window()
+        self.setCentralWidget(self.sw)
+        self.sw.show()
+        self.mw = Main_Window()
+        self.mw.setVisible(False)
+
+    def spin(self):
+        f = sqlite3.connect('Casino_database.db')
+        self.spin_error.setText('')
+        cur = f.cursor()
+        sl1 = choice(SLOTS_TYPES)
+        sl2 = choice(SLOTS_TYPES)
+        sl3 = choice(SLOTS_TYPES)
+        bet = int(self.bet_combobox.currentText())
+        # ошибка в случае если ставка превышает баланс
+        if bet > self.balance:
+            self.spin_error.setText('Не хватает средств')
+            self.troll_label.setText('')
+            self.info_label.setText('')
+            self.slot1.setText('.')
+            self.slot2.setText('.')
+            self.slot3.setText('.')
+            return None
+        cherry_count = 0
+        for i in (sl1, sl2, sl3):
+            if i == 'c':
+                cherry_count += 1
+
+        # 7 wins
+        if sl1 == sl2 == sl3 == '7':
+            result = bet * 1000
+            cur.execute(f"""UPDATE client_data SET balance = balance + {str(result)} - {str(bet)} 
+            WHERE login = '{self.login}'""")
+            f.commit()
+            self.balance = int(*cur.execute(f"""SELECT balance from client_data 
+                                    where login = '{self.login}'""").fetchone())
+            self.balance_label.setText(f'Ваш баланс: {self.balance}')
+            self.troll_label.setText(choice(VICTORY_PHRASES))
+            self.info_label.setText(f'Вы выйграли: {result}')
+        # bar wins
+        elif sl1 == sl2 == sl3 == 'bar':
+            result = bet * 300
+            cur.execute(f"""UPDATE client_data SET balance = balance + {str(result)} - {str(bet)} 
+            WHERE login = '{self.login}'""")
+            f.commit()
+            self.balance = int(*cur.execute(f"""SELECT balance from client_data 
+                                    where login = '{self.login}'""").fetchone())
+            self.balance_label.setText(f'Ваш баланс: {self.balance}')
+            self.troll_label.setText(choice(VICTORY_PHRASES))
+            self.info_label.setText(f'Вы выйграли: {result}')
+        # bell wins
+        elif sl1 == sl2 == sl3 == 'b':
+            result = bet * 80
+            cur.execute(f"""UPDATE client_data SET balance = balance + {str(result)} - {str(bet)} 
+            WHERE login = '{self.login}'""")
+            f.commit()
+            self.balance = int(*cur.execute(f"""SELECT balance from client_data 
+                                    where login = '{self.login}'""").fetchone())
+            self.balance_label.setText(f'Ваш баланс: {self.balance}')
+            self.troll_label.setText(choice(VICTORY_PHRASES))
+            self.info_label.setText(f'Вы выйграли: {result}')
+        # grape wins
+        elif sl1 == sl2 == sl3 == 'g':
+            result = bet * 50
+            cur.execute(f"""UPDATE client_data SET balance = balance + {str(result)} - {str(bet)} 
+            WHERE login = '{self.login}'""")
+            f.commit()
+            self.balance = int(*cur.execute(f"""SELECT balance from client_data 
+                                    where login = '{self.login}'""").fetchone())
+            self.balance_label.setText(f'Ваш баланс: {self.balance}')
+            self.troll_label.setText(choice(VICTORY_PHRASES))
+            self.info_label.setText(f'Вы выйграли: {result}')
+        # lemon wins
+        elif sl1 == sl2 == sl3 == 'l':
+            result = bet * 25
+            cur.execute(f"""UPDATE client_data SET balance = balance + {str(result)} - {str(bet)} 
+            WHERE login = '{self.login}'""")
+            f.commit()
+            self.balance = int(*cur.execute(f"""SELECT balance from client_data 
+                                    where login = '{self.login}'""").fetchone())
+            self.balance_label.setText(f'Ваш баланс: {self.balance}')
+            self.troll_label.setText(choice(VICTORY_PHRASES))
+            self.info_label.setText(f'Вы выйграли: {result}')
+        # cherry3 wins
+        elif cherry_count == 3:
+            result = bet * 100
+            cur.execute(f"""UPDATE client_data SET balance = balance + {str(result)} - {str(bet)} 
+            WHERE login = '{self.login}'""")
+            f.commit()
+            self.balance = int(*cur.execute(f"""SELECT balance from client_data 
+                                    where login = '{self.login}'""").fetchone())
+            self.balance_label.setText(f'Ваш баланс: {self.balance}')
+            self.troll_label.setText(choice(VICTORY_PHRASES))
+            self.info_label.setText(f'Вы выйграли: {result}')
+        # cherry2 wins
+        elif cherry_count == 2:
+            result = bet * 25
+            cur.execute(f"""UPDATE client_data SET balance = balance + {str(result)} - {str(bet)} 
+            WHERE login = '{self.login}'""")
+            f.commit()
+            self.balance = int(*cur.execute(f"""SELECT balance from client_data 
+                                    where login = '{self.login}'""").fetchone())
+            self.balance_label.setText(f'Ваш баланс: {self.balance}')
+            self.troll_label.setText(choice(VICTORY_PHRASES))
+            self.info_label.setText(f'Вы выйграли: {result}')
+        # cherry1 wins
+        elif cherry_count == 1:
+            result = bet * 2
+            cur.execute(f"""UPDATE client_data SET balance = balance + {str(result)} - {str(bet)} 
+            WHERE login = '{self.login}'""")
+            f.commit()
+            self.balance = int(*cur.execute(f"""SELECT balance from client_data 
+                                    where login = '{self.login}'""").fetchone())
+            self.balance_label.setText(f'Ваш баланс: {self.balance}')
+            self.troll_label.setText(choice(VICTORY_PHRASES))
+            self.info_label.setText(f'Вы выйграли: {result}')
+        else:
+            cur.execute(f"""UPDATE client_data SET balance = balance - {str(bet)} 
+                        WHERE login = '{self.login}'""")
+            f.commit()
+            self.balance = int(*cur.execute(f"""SELECT balance from client_data 
+                                                where login = '{self.login}'""").fetchone())
+            self.balance_label.setText(f'Ваш баланс: {self.balance}')
+            self.troll_label.setText(choice(TROLL_PHRASES))
+            self.info_label.setText(f'Вы проиграли')
+        self.slot1.setText(sl1)
+        self.slot2.setText(sl2)
+        self.slot3.setText(sl3)
 
 
 
